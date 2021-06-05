4d1b41ad3f4abec9cf0cecbac04545eed71052c3
marlontamo <marlontamo@gmail.com>
2021-06-05 17:48:09 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <b>
            Welcome to my react testapp
-        </p>
+        </b>
        
       </header>
     </div>
